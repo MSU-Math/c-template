c0e199e075e69b26d77689a0ea9918587d5022dd
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2020-08-31 12:54:41 +0300

    lint.sh: propagate errors

--- .github/lint.sh
+++ .github/lint.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 ERRORS=0
 function countError {
-    echo "Программа завершилась с ошибкой:"
+    echo "Программа завершилась с ошибкой."
     (( ERRORS++ ))
 }
 trap countError ERR 
 
 echo "# cppcheck"
-cppcheck --enable=warning,performance --error-exitcode=2 . && echo "OK"
+cppcheck --enable=warning,performance --error-exitcode=2 . && echo "OK" || false
 
 echo "# clang-tidy"
-clang-tidy --checks=clang-analyzer-*,bugprone-*,performance-*,readability-*,misc-* --warnings-as-errors=* *.c -- && echo "OK"
+clang-tidy --checks=clang-analyzer-*,bugprone-*,performance-*,readability-*,misc-* --warnings-as-errors=* *.c -- && echo "OK" || false
 
 echo "# clang-format (форматирование кода)"
-clang-format --style="{BasedOnStyle: llvm, IndentWidth: 4, AlignTrailingComments: false, BreakBeforeBraces: Linux, AllowShortFunctionsOnASingleLine: Inline}" --dry-run -Werror ./*.c && echo "OK"
+clang-format --style="{BasedOnStyle: llvm, IndentWidth: 4, AlignTrailingComments: false, BreakBeforeBraces: Linux, AllowShortFunctionsOnASingleLine: Inline}" --dry-run -Werror ./*.c && echo "OK" || false
 
 echo "# Предупреждения GCC"
-gcc -Wall -Wextra -Werror -lm -g -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all ./*.c && echo "OK"
+gcc -Wall -Wextra -Werror -lm -g -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=null -fsanitize=bounds-strict -fstack-protector-all ./*.c  && echo "OK" || false
 
 exit $ERRORS
